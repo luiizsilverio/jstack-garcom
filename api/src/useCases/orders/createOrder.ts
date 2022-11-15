7d2f381f32006c8d105e9ed9f08f24c98e3c8eb3
luiizsilverio <luiiz.silverio@gmail.com>
2022-11-15 17:59:51 -0300

    Aula 1 (3:03)

---
+++ api/src/useCases/orders/createOrder.ts
@@ -0,0 +1,26 @@
+import { Request, Response } from "express";
+import { Order } from "../../models/order";
+
+export async function createOrder(req: Request, res: Response) {
+  const { table, products } = req.body;
+
+  try {
+    const order = await Order.create({
+      table,
+      products: products ? JSON.parse(products): []
+    });
+
+    res.status(201).json(order);
+  }
+  catch (err) {
+    if (err instanceof Error) {
+      res.status(500).json({
+        error: err.message
+      })
+    } else {
+      res.status(500).json({
+        error: 'Internal Error'
+      })
+    }
+  }
+}
